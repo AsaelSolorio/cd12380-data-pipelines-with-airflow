802bddc17dd012fd337506f008533b66450c19f9
AsaelSolorio <112660076+AsaelSolorio@users.noreply.github.com>
2024-07-08 11:38:20 -0600

    Update load_dimension.py

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -8,15 +8,20 @@ class LoadDimensionOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 conn_id,
+                 table,
+                 query,
+                 truncate = False,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.conn_id = conn_id
+        self.table = table
+        self.query = query
+        self.truncate = truncate
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=conn_id)
+        if self.truncate:
+            redshift.run(f"TRUNCATE TABLE {self.table}")
+        redshift.run(f"INSERT INTO {self.table} {self.query}")

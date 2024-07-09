c974d5fb99dacda13a9e26188d93a2c55c06c34e
AsaelSolorio <112660076+AsaelSolorio@users.noreply.github.com>
2024-07-09 09:48:49 -0600

    Update load_dimension.py

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -2,26 +2,30 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+
 class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
 
     @apply_defaults
     def __init__(self,
-                 conn_id,
-                 table,
-                 query,
-                 truncate = False,
+                 redshift_conn_id="",
+                 sql_query="",
+                 table="",
+                 mode="append",
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
         self.table = table
-        self.query = query
-        self.truncate = truncate
+        self.sql_query = sql_query
+        self.mode = mode
 
     def execute(self, context):
-        redshift = PostgresHook(postgres_conn_id=conn_id)
-        if self.truncate:
-            redshift.run(f"TRUNCATE TABLE {self.table}")
-        redshift.run(f"INSERT INTO {self.table} {self.query}")
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        self.log.info("Loading data from staging table to dimension table")
+        if self.mode == "truncate-insert":
+            self.log.info("Truncating dimension table")
+        redshift.run("TRUNCATE TABLE {}".format(self.table))
+        self.log.info("Inserting data into dimension table")
+        redshift.run("INSERT INTO {} {}".format(self.table, self.sql_query))
